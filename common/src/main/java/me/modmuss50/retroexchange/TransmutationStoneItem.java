0a88f8bd9bdf25890e6f09ccf8fe0f4b83d24778
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-11-03 08:29:16 +0100

    rework item damaging

--- common/src/main/java/me/modmuss50/retroexchange/TransmutationStoneItem.java
+++ common/src/main/java/me/modmuss50/retroexchange/TransmutationStoneItem.java
@@ -3,7 +3,6 @@ package me.modmuss50.retroexchange;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.ChatFormatting;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.InteractionResult;
@@ -13,7 +12,7 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Block;
 
 import java.util.List;
 
@@ -31,49 +30,24 @@ public class TransmutationStoneItem extends Item implements ExtendedRecipeRemain
 	// FIXME: not functional on Forge
 	@Override
 	public ItemStack getRemainderStack(ItemStack stack) {
-		damage(stack);
-		if (getDamage(stack) >= maxDamage) {
+		stack.setDamageValue(stack.getDamageValue() + 1);
+
+		if (stack.getDamageValue() >= stack.getMaxDamage()) {
 			return ItemStack.EMPTY;
 		}
+
 		return stack;
-	}
-
-	public static int getDamage(ItemStack stack) {
-		checkTag(stack);
-		return stack.getTag().getInt("Damage");
-	}
-
-	public static ItemStack damage(ItemStack stack) {
-		checkTag(stack);
-		stack.getTag().putInt("Damage", getDamage(stack) + 1);
-		return stack;
-	}
-
-	private static void checkTag(ItemStack stack) {
-		if (!stack.hasTag()) {
-			stack.setTag(new CompoundTag());
-		}
-		if (!stack.getTag().contains("Damage")) {
-			stack.getTag().putInt("Damage", 0);
-		}
 	}
 
 	@Override
 	public InteractionResult useOn(UseOnContext context) {
-		BlockState blockState = context.getLevel().getBlockState(context.getClickedPos());
+		Level level = context.getLevel();
+		Block block = level.getBlockState(context.getClickedPos()).getBlock();
 
-		if (BlockExchangeManager.INSTANCE.blockConversionMap.containsKey(blockState.getBlock())) {
-			if (!context.getLevel().isClientSide()) {
-				context.getLevel().setBlock(context.getClickedPos(), BlockExchangeManager.INSTANCE.blockConversionMap.get(blockState.getBlock()).defaultBlockState(), 3);
-
-				ItemStack stack = context.getPlayer().getItemInHand(context.getHand()).copy();
-				damage(stack);
-
-				if (getDamage(stack) >= MAX_DAMAGE) {
-					stack.setCount(0);
-				}
-
-				context.getPlayer().setItemInHand(context.getHand(), stack);
+		if (BlockExchangeManager.INSTANCE.blockConversionMap.containsKey(block)) {
+			if (!level.isClientSide) {
+				level.setBlockAndUpdate(context.getClickedPos(), BlockExchangeManager.INSTANCE.blockConversionMap.get(block).defaultBlockState());
+				context.getItemInHand().hurtAndBreak(1, context.getPlayer(), player -> player.broadcastBreakEvent(context.getHand()));
 			}
 
 			return InteractionResult.SUCCESS;
@@ -85,7 +59,7 @@ public class TransmutationStoneItem extends Item implements ExtendedRecipeRemain
 	@Override
 	@Environment(EnvType.CLIENT)
 	public void appendHoverText(ItemStack stack, Level world, List<Component> tooltip, TooltipFlag tooltipOptions) {
-		tooltip.add(new TextComponent("Uses Left: ").append(new TextComponent(String.valueOf(MAX_DAMAGE - stack.getDamageValue()))).withStyle(ChatFormatting.GREEN));
+		tooltip.add(new TextComponent("Uses Left: ").append(new TextComponent(String.valueOf(stack.getMaxDamage() - stack.getDamageValue()))).withStyle(ChatFormatting.GREEN));
 	}
 
 	@Override
